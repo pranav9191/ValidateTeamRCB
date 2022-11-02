399c1220d47db74233f7d771cae16c5a5390beeb
PRATIK <PRATIK@PRATIK-PC>
2022-11-02 23:53:23 +0530

    Code Changes
    -Separate test case for validation
    -code arranged for re-usability
    -extent report and test listener implemented

--- TestVagrantCodingAssignment/src/test/java/testpackage/ValidateTeamRCB.java
+++ TestVagrantCodingAssignment/src/test/java/testpackage/ValidateTeamRCB.java
@@ -1,38 +1,40 @@
 package testpackage;
 
-import java.io.FileReader;
-import java.util.Iterator;
-import org.json.simple.JSONArray;
+import java.util.List;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
+import basepackage.ReadJSON;
+
 public class ValidateTeamRCB {
-  @SuppressWarnings({ "unused", "rawtypes" })
+	  String caseid;
+  @SuppressWarnings({ "unused", "rawtypes", "unchecked" })
+  
+  List<JSONObject> players;
+  
+@BeforeClass
+public void readRCBTeam()
+{
+	  players = ReadJSON.getRCBPlayers("target\\TeamRCB.json");
+}
+  
 @Test
-  public void validateTeamRCB() {
-		JSONParser parser = new JSONParser();
-        int wicketKeepers = 0, foreignPlayers = 0;
-		try
-		{
-			Object object = parser.parse(new FileReader("target\\TeamRCB.json"));
-			JSONObject jb = (JSONObject)object;
-			JSONArray players = (JSONArray)jb.get("player");
-	        Iterator iterator = players.iterator();
-	         while (iterator.hasNext()) {
-	        	 JSONObject childObject = (JSONObject) iterator.next();
-	        	 if(childObject.get("role").toString().equals("Wicket-keeper")) wicketKeepers++;
-	        	 if(!(childObject.get("country").toString().equals("India"))) foreignPlayers++;
-	         }
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-		}
+  public void validateTeamRCBWicketKeepers() {
+		int wicketKeepers = ReadJSON.getWicketKeepers(players);
 		SoftAssert softAssert = new SoftAssert();
-		softAssert.assertTrue(wicketKeepers >=1);
-		softAssert.assertTrue(foreignPlayers<=4);
+		softAssert.assertTrue(wicketKeepers >=1,"team have wicketkeeper");
 		softAssert.assertAll();
   }
+
+@Test
+public void validateTeamRCBForeignPlayers() {
+		int foreignPlayers = ReadJSON.getWicketKeepers(players);
+		SoftAssert softAssert = new SoftAssert();
+		softAssert.assertTrue(foreignPlayers <=4,"team have wicketkeeper");
+		softAssert.assertAll();
 }
+
+}
+
